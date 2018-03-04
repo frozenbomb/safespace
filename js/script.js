040d1a75bb536383a4b6564a628f66616261553b
Glenn-Jaya <jayasugp@myumanitoba.ca>
2018-03-03 18:42:47 -0600

    Added dogPFP and animate css file

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
 		else {
 			replied = "I will see what i can do";
 		}
-		$("#chat_box").append('<div class="message"><img class="user-img" src="assets/ic_account_circle_black_48px.svg"><div class="msg-content"><h4 class="user-name">Dog</h4><p class="user-msg">' + replied + '</p></div></div>');
+		$("#chat_box").append('<div class="message"> <img class="dogPFP user-img animated zoomIn" src="http://img15.deviantart.net/257d/i/2013/276/4/a/lovin_oven_dog_by_mannydragon5-d6p3i1h.png"><div class="msg-content"><h4 class="user-name">Dog</h4><p class="user-msg">' + replied + '</p></div></div>');
 
 		$("#chat_box").scrollTop($("#chat_box")[0].scrollHeight);
 	}
@@ -224,5 +224,6 @@ $(document).ready(function() {
 		        reader.readAsDataURL(dataTransfer.files[0]);
 	    	}
 		}
+		//$(".user-img").addClass("animated bounce");
 	});
 });
